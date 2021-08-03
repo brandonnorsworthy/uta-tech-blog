045d781c4de48ec34425b6b795a7a804782018c1
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-03 11:21:42 -0500

    seeds are working completely

--- seeds/Comment.js
+++ seeds/Comment.js
@@ -4,133 +4,133 @@ const commentdata = [
     {
         date_created:"4/9/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:1,
+        user_id:1,
         post_id:1
     },
     {
         date_created:"5/13/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:3,
+        user_id:3,
         post_id:2
     },
     {
         date_created:"5/14/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:5,
+        user_id:5,
         post_id:2
     },
     {
         date_created:"5/16/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:7,
+        user_id:7,
         post_id:2
     },
     {
         date_created:"5/16/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:1,
+        user_id:1,
         post_id:2
     },
     {
         date_created:"7/14/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:5,
+        user_id:5,
         post_id:3
     },
     {
         date_created:"9/8/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:6,
+        user_id:6,
         post_id:4
     },
     {
         date_created:"11/3/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:7,
+        user_id:7,
         post_id:5
     },
     {
         date_created:"11/4/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:8,
+        user_id:8,
         post_id:5
     },
     {
         date_created:"11/5/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:9,
+        user_id:9,
         post_id:5
     },
     {
         date_created:"11/6/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:10,
+        user_id:10,
         post_id:5
     },
     {
         date_created:"12/25/2020",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:4,
+        user_id:4,
         post_id:6
     },
     {
         date_created:"2/11/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:5,
+        user_id:5,
         post_id:6
     },
     {
         date_created:"2/12/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:4,
+        user_id:4,
         post_id:6
     },
     {
         date_created:"3/8/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:3,
+        user_id:3,
         post_id:7
     },
     {
         date_created:"3/12/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:2,
+        user_id:2,
         post_id:7
     },
     {
         date_created:"3/14/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:1,
+        user_id:1,
         post_id:7
     },
     {
         date_created:"5/19/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:9,
+        user_id:9,
         post_id:8
     },
     {
         date_created:"5/19/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:6,
+        user_id:6,
         post_id:9
     },
     {
         date_created:"5/20/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:1,
+        user_id:1,
         post_id:9
     },
     {
         date_created:"5/26/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:4,
+        user_id:4,
         post_id:10
     },
     {
         date_created:"5/27/2021",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus. Porttitor rhoncus dolor purus non. Non tellus orci ac auctor augue mauris augue neque gravida. Facilisis leo vel fringilla est ullamcorper. Ultrices sagittis orci a scelerisque purus. Mi in nulla posuere sollicitudin aliquam ultrices. Nec dui nunc mattis enim ut. Cras sed felis eget velit aliquet sagittis. Blandit cursus risus at ultrices mi tempus. Elit ullamcorper dignissim cras tincidunt. Semper quis lectus nulla at volutpat.",
-        author_id:5,
+        user_id:5,
         post_id:10
     },
 ];
--- seeds/Post.js
+++ seeds/Post.js
@@ -4,61 +4,61 @@ const postdata = [
     {
         title:"the sjws have taken everything from me. my dignity. my happiness. and they pretend to like bad things",
         content:"Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor hodor; hodor hodor - hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor; hodor hodor; hodor hodor hodor! Hodor hodor HODOR! Hodor hodor",
-        author_id:2,
+        user_id:2,
         date_created: "4/9/2020"
     },
     {
         title:"i think we take it for granted that i illegally torrent communism",
         content:"We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen.",
-        author_id:4,
+        user_id:4,
         date_created: "5/12/2020"
     },
     {
         title:"im horrible. im terrible. i pretend to like mysterious unsourced meat",
         content:"Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag.",
-        author_id:4,
+        user_id:4,
         date_created: "7/14/2020"
     },
     {
         title:"we need to start a fundraiser in order to fund fursonas",
         content:"De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.	",
-        author_id:4,
+        user_id:4,
         date_created: "9/8/2020"
     },
     {
         title:"it turns out that vaccines actually cause you to think about death",
         content:"Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye.	",
-        author_id:6,
+        user_id:6,
         date_created: "11/3/2020"
     },
     {
         title:"im not a person. im a bunch of legos and i endorse it",
         content:"Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah croissant candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake. Muffin danish chocolate soufflé pastry icing bonbon oat cake.	",
-        author_id:6,
+        user_id:6,
         date_created: "12/24/2020"
     },
     {
         title:"slugs dont spy on dragons",
         content:"Just say anything, George, say what ever's natural, the first thing that comes to your mind. Take that you mutated son-of-a-bitch. My pine, why you. You space bastard, you killed a pine. You do? Yeah, it's 8:00. Hey, McFly, I thought I told you never to come in here.",
-        author_id:7,
+        user_id:7,
         date_created: "2/11/2021"
     },
     {
         title:"do not fear for the end is coming soon. we can revel in the thick clotted mud of the fields. we can hide from adam sandler?",
         content:"Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.	",
-        author_id:7,
+        user_id:7,
         date_created: "3/8/2021"
     },
     {
         title:"a little known fact is that there is a worm version of every holiday. for example on worm valentines day we celebrate worm love and create shrimp",
         content:"Busey ipsum dolor sit amet. Have you urinated? Have you drained your bladder? Are you free? Because if you haven't it will only come out later. I'm giving you some information that your bodily fluids may penetrate your clothing fibre's without warning.It's OK to get Rib-grease on your face, because you're allowing people to see that you're proud of these ribs.",
-        author_id:8,
+        user_id:8,
         date_created: "5/19/2021"
     },
     {
         title:"do not fear for the end is coming soon",
         content:"Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.	",
-        author_id:9,
+        user_id:9,
         date_created: "5/26/2021"
     }
 ];
