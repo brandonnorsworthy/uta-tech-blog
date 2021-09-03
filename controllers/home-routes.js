9610ca8799d6587ca9bcbd25f371b0a9e1e83710
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-03 18:06:31 -0500

    formatted navbar and models are finished

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,12 +6,18 @@ const withAuth = require('../utils/auth');
 // GET all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    console.log('hit')
-    const dbPostData = await Post.findAll();
+    const dbPostData = await Post.findAll({
+      limit: 50,
+      include: [
+        { model: User },
+      ],
+    });
 
     const posts = dbPostData.map((gallery) =>
       gallery.get({ plain: true })
     );
+
+    console.log(posts[0])
 
     res.render('homepage', {
       posts: posts,
@@ -25,41 +31,12 @@ router.get('/', async (req, res) => {
 
 // GET one gallery
 // Use the custom middleware before allowing the user to access the gallery
-router.get('/gallery/:id', withAuth, async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
   try {
-    const dbGalleryData = await Gallery.findByPk(req.params.id, {
-      include: [
-        {
-          model: Painting,
-          attributes: [
-            'id',
-            'title',
-            'artist',
-            'exhibition_date',
-            'filename',
-            'description',
-          ],
-        },
-      ],
-    });
+    const dbPostData = await Post.findByPk(req.params.id);
 
-    const gallery = dbGalleryData.get({ plain: true });
+    const posts = dbPostData.get({ plain: true });
     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET one painting
-// Use the custom middleware before allowing the user to access the painting
-router.get('/painting/:id', withAuth, async (req, res) => {
-  try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
-
-    const painting = dbPaintingData.get({ plain: true });
-
-    res.render('painting', { painting, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
