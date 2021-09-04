afcf14432206920d3ebd84a01c35fd4656426bbd
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-03 23:56:55 -0500

    dashboard is clickable

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -36,5 +36,26 @@ const createPost = async (event) => {
   }
 }
 
+const selectPost = async (event) => {
+  var target = getLiTag(event.target)
+
+  document.location.replace(`/post/${target.id}`);
+}
+
+function getLiTag(target) {
+  if (!(target.tagName === 'LI')) {
+    target = getLiTag(target.parentElement)
+  }
+  return target;
+}
+
+var tags = document.getElementsByTagName("li")
+for (const key in tags) {
+  if (tags[key].tagName === 'LI') {
+    tags[key].addEventListener('click', selectPost);
+  }
+}
+
+
 document.querySelector('#new-post').addEventListener('click', showCreatePost);
 document.querySelector('.create-post-form').addEventListener('submit', createPost);
